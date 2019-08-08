771977956ad082eccd88fb628869915934f8b65c
Anson Kwok <ansonkwok0@gmail.com>
2019-08-08 12:27:42 +1000

    Deploy website

--- question-answer/src/app/app.component.ts
+++ question-answer/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'question-answer';
+  question = true;
+  text = "Why was the JavaScript developer sad?";
+  btnText = "Answer";
+
+  onQuestion() {
+    if (this.question == true) {
+      //change text
+      this.text = "Why was the JavaScript developer sad?" + "\n" + "Because he didnt Node how to Express himself!";
+      //change button text 
+      this.btnText = "Again!";
+      this.question = false;
+    } else {
+      //change text
+      this.text = "Why was the JavaScript developer sad?";
+      //change button text
+      this.btnText = "Answer";
+      this.question = true;
+    }
+  }
 }
